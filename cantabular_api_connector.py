9ef0185c87e68270e6e27b943186e05b3a784ee6
elias <43386424+eliaspercy@users.noreply.github.com>
2021-03-25 03:54:18 +0000

    Improved logging, tidied types and docstrings, addition of debug flag
    The logging functionality of the program has been improved such that now only relevant events are logged to the file, including any exceptions that halt the program.  The verbose flag has been altered so as to not print out literally everything, and the debug flag was added to do just that.

--- cantabular_api_connector.py
+++ cantabular_api_connector.py
@@ -7,34 +7,55 @@ logger = getLogger('DTS-Logger')
 
 
 class CantabularApiConnector(ApiConnector, DataSource):
-    """Class for communicating directly with the Cantabular API, in order to retrieve a jsonstat dataset based on
-    specific queries.
+    """
+    Class for communicating directly with the Cantabular API, in order to retrieve a jsonstat dataset based on
+    specific queries. Currently only configured to work with data (i.e., not metadata).
 
-    :param dataset: Name/ID of a dataset
+    :param dataset: Name/ID of a dataset to retrieve from the Cantabular system.
     :param variables: A list containing valid variables.
-    :ivar query_url: url with endpoints derived from params dataset and variables
+    :ivar query_url: URL with endpoints derived from params dataset and variables.
+    :vartype query_url: str
 
     """
     def __init__(self, dataset: str, variables: list, credentials, address, port=None) -> None:
         super().__init__(credentials, address, port)
 
+        self.dataset = dataset
+        self.variables = variables
+
         # Construct the query url with endpoints using base url (client), dataset and variables.
         self.query_url = self.client + '/v8/query-json-stat/%s?%s' % (dataset, '&'.join([f'v={v}' for v in variables]))
 
     def query(self) -> pyjstat.Dataset:
-        """Method for making a query to the Cantabular API using the argument variables.
+        """
+        Method for making a query to the Cantabular API using the argument variables. This is the Cantabular API
+            connector version of the query() abstract method, inherited from the `DataSource` class.
+
         :raises requests.HTTPError: Raised in the case of a network partition or invalid query to the Cantabular API.
-        :return: A cantabular table in the form of a jsonstat dataframe
+        :return: A cantabular table in the form of a jsonstat dataframe.
         """
         try:
+            logger.debug(f"Attempting to connect to the Cantabular API at {self.client}.")
             res = self.session.get(self.query_url)
+            logger.info(f"Connection successfully established with the Cantabular API at {self.client}.")
         except Exception as e:
             raise requests.ConnectionError(f"Unable to connect to client. ({e})")
 
         # Check for an errored response. This may occur if the query contained invalid values, or if the entire output
         # table was blocked for disclosure control reasons.
-        if not res.ok:
-            raise requests.HTTPError(f'Bad response from the Cantabular server, it may be down. ({res.content})')
+        if res.status_code == 404:
+            raise requests.HTTPError(f"Bad response from the Cantabular API: "
+                                     f"the dataset '{self.dataset}' does not exist.")
+        elif res.status_code == 400:
+            raise requests.HTTPError(f'Bad response from the Cantabular API: the variables are either incorrect or'
+                                     f'invalid. ({res.json()["message"]}.)')
+        elif res.status_code == 401:
+            raise requests.HTTPError(f'Bad response from the Cantabular API: the credentials were not accepted.'
+                                     f'({res.json()["message"]}.)')
+        elif not res.ok:
+            raise requests.HTTPError(f'Bad response from the Cantabular API: {res.text}.')
+
+        logger.info(f"{self.dataset} dataset with variables {self.variables} retrieved successfully.")
 
         # Load response into a pyjstat dataframe.
         return self.load_jsonstat(res.content.decode('utf-8'))
--- dataset_transformations.py
+++ dataset_transformations.py
@@ -1,42 +1,54 @@
 from type_hints import *
 from pyjstat import pyjstat
+from logging import getLogger
+
+logger = getLogger("DTS-Logger")
 
 
 class DatasetTransformations:
-    """Class containing methods for transforming the data retrieved from Cantabular
+    """
+    Class containing methods for transforming the data retrieved from Cantabular such that it is ready for transmission
+    to the Nomis system.
 
-    :param table: a pyjstat dataframe containing the data to be used/transformed
-    :ivar table: initial value: table
+    :param table: A pyjstat dataframe containing the data to be used/transformed.
+    :ivar table: Initial value: table.
+    :vartype table: Dataset
     """
 
-    def __init__(self, geography_flag: bool, table: pyjstat.Dataset = None, table_geography: pyjstat.Dataset = None):
+    def __init__(self, table: pyjstat.Dataset,
+                 geography_flag: bool = False,
+                 table_geography: Union[pyjstat.Dataset, None] = None) -> None:
         self.table = table
         self.geography_flag = geography_flag
         self.table_geography = table_geography
         self.validate_table()
 
-
-
     def validate_table(self):
-        """Method for validating the table, ensuring it is of the correct type and contains sufficient keys
+        """
+        Method for validating the table, ensuring it is of the correct type and contains sufficient keys.
+
+        :raises TypeError: If the dataset is not of the correct type, i.e., a pyjstat Dataset.
+        :raises LookupError: If the table is devoid of a "dimension" key, which is required for the transformations.
         """
         if not isinstance(self.table, pyjstat.Dataset):
             raise TypeError(f"Table was detected as type {type(self.table)}. "
                             f"This is invalid, the table must be a pyjstat.Dataset.")
         if "dimension" not in self.table:
             raise KeyError("Table supplied contains no dimensions key.")
+        logger.debug("Table validated successfully.")
 
     @staticmethod
     def dataset_creation(dataset_id: str, dataset_title: str) -> NomisDataset:
-        """Method for initialising a Nomis-style dataset using an inputted ID and title.
+        """
+        Method for initialising a Nomis-style dataset using an inputted ID and title.
 
-        :param dataset_id: A valid string representing the ID of the dataset to be created
-        :param dataset_title: A valid string representing the title of the dataset to be created
+        :param dataset_id: A valid string representing the ID of the dataset to be created.
+        :param dataset_title: A valid string representing the title of the dataset to be created.
 
-        :raises TypeError: If dataset_id or dataset_title are not valid strings (str types)
-        :raises ValueError: If dataset_id or dataset_title have a length of 0 (i.e., are empty)
+        :raises TypeError: If `dataset_id` or `dataset_title` are not valid strings (str types).
+        :raises ValueError: If `dataset_id` or `dataset_title` have a length of 0 (i.e., are empty).
 
-        :return: If no exception is raised, an initialised dataset as a Python dict with valid keys and values
+        :return: If no exception is raised, an initialised dataset as a Python dict with valid keys and values.
         """
         if not isinstance(dataset_id, str):
             raise TypeError(f"The dataset id (inputted: {dataset_id}) must be a string.")
@@ -58,25 +70,30 @@ class DatasetTransformations:
             "minimumRound": 0,
             "online": True
         }
+        logger.debug("Initialised the dataset in the Nomis format.")
         return ds
 
     def variable_creation(self) -> List[Variables]:
-        """Method for converting the variable data from the pyjstat dataframe into valid variables for communication
-        with the Nomis API
-
-        :raises KeyError: If the dataframe doesn't contain any dimension information
-        :return: A list of variables
         """
-        requests = [{
-            "name": dimension,
-            "label": self.table["dimension"][dimension]["label"],
-            "defaults": None
-        }
-            for dimension in self.table["dimension"]]
+        Method for converting the variable data from the pyjstat dataframe into valid variables for communication
+        with the Nomis API.
+
+        :return: A list of variables.
+        """
+        requests = [
+            {
+                "name": dimension,
+                "label": self.table["dimension"][dimension]["label"],
+                "defaults": None
+            }
+            for dimension in self.table["dimension"]
+        ]
+        logger.debug("Prepared the variable requests.")
         return requests
 
     def type_creation(self) -> List[Variables]:
-        """Method for creating a valid variable type using the pyjstat dataframe  for communication
+        """
+        Method for creating a valid variable type using the pyjstat dataframe for communication
         with the Nomis API
 
         :raises KeyError: If the dataframe doesn't contain any dimension information
@@ -90,14 +107,18 @@ class DatasetTransformations:
                     "reference": dimension,
                     "title": self.table["dimension"][dimension]["label"],
                     "titlePlural": self.table["dimension"][dimension]["label"],
-                }           
+                }
             )
+        logger.debug("Prepared types requests.")
         return requests
 
-    def category_creation(self, type_ids) -> List[Variables]:
-        """Method for constructing a list of variable categories, using the jsonstat table retrieved from cantabular
-        :return: A list of variable categories
+    def category_creation(self, type_ids: List[str]) -> List[Variables]:
         """
+        Method for constructing a list of variable categories, using the jsonstat table retrieved from cantabular.
+
+        :return: A list of variable categories.
+        """
+
         if not isinstance(type_ids, list):
             raise TypeError("Invalid type_ids param, must be a list.")
 
@@ -118,12 +139,18 @@ class DatasetTransformations:
                     }
                 )
             counter += 1
+        logger.debug("Prepared category requests.")
         return requests
 
     def assign_dimensions(self, key: str) -> List[Dimensions]:
-        """Method for using the jsonstat table to construct a list of dimensions, based on the initial query to
-        cantabular
-        :return: A list of dataset dimensions
+        """
+        Method for using the jsonstat table to construct a list of dimensions, based on the initial query to
+        cantabular.
+
+        :param key: The key in the pyjstat dataframe corresponding with the dimensions/variables to be appended to the
+            dataset.
+
+        :return: A list of dataset dimensions.
         """
 
         requests = []
@@ -140,7 +167,7 @@ class DatasetTransformations:
                     },
                     "role": "Spatial",
                     "canFilter": True,
-                    "defaults": None, #self.table["dimension"][dimension]["category"]["index"],
+                    "defaults": None,
                     "database": {
                         "isKey": True,
                         "index": index,
@@ -149,7 +176,7 @@ class DatasetTransformations:
                     },
                 }
             )
-            index += 1            
+            index += 1
 
         for dimension in self.table["dimension"]:
             if dimension == key:
@@ -177,14 +204,20 @@ class DatasetTransformations:
                 }
             )
             index += 1
-            
+
+        logger.debug("Prepared dimensions requests.")
         return requests
 
     def observations(self, dataset_id: str) -> Observations:
-        """Method for creating dataset observations
+        """
+        Method for creating dataset observations.
 
-        :param dataset_id: A valid dataset ID
-        :return: A python dict representing dataset dimensions
+        :param dataset_id: A valid dataset ID, which must be a nonempty string.
+
+        :raises TypeError: If the `dataset_id` is not a string.
+        :raises ValueError: If the `dataset_id` is an empty string.
+
+        :return: A python dict representing dataset dimensions.
         """
         if not isinstance(dataset_id, str):
             raise TypeError(f"The dataset id (inputted: {dataset_id}) must be a string.")
@@ -210,6 +243,7 @@ class DatasetTransformations:
             dimensions.append(dimension)
             codes.append(data["dimension"][dimension]["category"]["index"])
 
+        logger.debug("Prepared observations.")
         return (
             {
                 "dataset": dataset_id,
@@ -222,35 +256,43 @@ class DatasetTransformations:
 
     @staticmethod
     def variable_metadata_request(uuids_metadata: List[UuidMetadata]) -> list:
-        """Method for the construction of metadata in a format ready to be transmitted to Nomis
-        :param uuids_metadata: A list of namedtuples, each containing a uuid and some metadata associated with that uuid
-        :return: A list of metadata, ready to be sent to the Nomis system
+        """
+        Method for the construction of metadata in a format ready to be transmitted to Nomis.
+
+        :param uuids_metadata: A list of namedtuples, each containing a UUID and some metadata associated with that UUID.
+
+        :raises TypeError: If the uuids_metadata passed is not a list, or any of the elements contained in uuids_metadata
+            is not a valid instance of the UuidMetadata namedtuple.
+
+        :return: A list of metadata, ready to be sent to the Nomis system.
         """
         if not isinstance(uuids_metadata, list):
             raise TypeError("Parameter must be in a list.")
         for elem in uuids_metadata:
             if not isinstance(elem, UuidMetadata):
                 raise TypeError("Parameter contains invalid types.")
-        requests = [{
-            "id": None,
-            "belongsTo": id_md.uuid,
-            "description": None,
-            "created": None,
-            "validFrom": None,
-            "validTo": None,
-            "include": None,
-            "meta": [
-                {
-                    "role": "note",
-                    "properties": [
-                        {
-                            "prefix": "dc",
-                            "property": "description",
-                            "value": id_md.metadata["description"]
-                        }
-                    ]
-                }
-            ]
-        }
+        requests = [
+            {
+                "id": None,
+                "belongsTo": id_md.uuid,
+                "description": None,
+                "created": None,
+                "validFrom": None,
+                "validTo": None,
+                "include": None,
+                "meta": [
+                    {
+                        "role": "note",
+                        "properties": [
+                            {
+                                "prefix": "dc",
+                                "property": "description",
+                                "value": id_md.metadata["description"]
+                            }
+                        ]
+                    }
+                ]
+            }
             for id_md in uuids_metadata]
+        logger.debug("Prepared metadata.")
         return requests
